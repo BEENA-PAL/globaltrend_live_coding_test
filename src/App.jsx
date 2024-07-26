7b7a5a4c62f06ed18b7af8454e1d80f2fea3d4e5
BEENA-PAL <129384052+BEENA-PAL@users.noreply.github.com>
2024-07-26 18:16:45 +0530

    navbar functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <div>
+      {/* when header is added */}
       <Header />
       <h1>Welcome To My Website</h1>
     </div>
